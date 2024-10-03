aef69bf366853d464881777298a49db3e57a04d4
Guilherme Silva <133605425+GuilhermeSP01@users.noreply.github.com>
2024-10-03 07:39:20 -0300

    add database verification for authentication request

--- client/src/main/java/com/guilherme/todo_app/model/users/User.java
+++ client/src/main/java/com/guilherme/todo_app/model/users/User.java
@@ -1,4 +1,4 @@
-package com.guilherme.todo_app.users;
+package com.guilherme.todo_app.model.users;
 
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
@@ -18,26 +18,38 @@ public class User {
     public Integer getId() {
         return id;
     }
+
     public void setId(Integer id) {
         this.id = id;
     }
+
     public String getEmail() {
         return email;
     }
+
     public void setEmail(String email) {
         this.email = email;
     }
+
     public String getUsername() {
         return username;
     }
+
     public void setUsername(String username) {
         this.username = username;
     }
+
     public String getPassword() {
         return password;
     }
+    
     public void setPassword(String password) {
         this.password = password;
+    }
+    
+    @Override
+    public String toString() {
+        return "User [id=" + id + ", email=" + email + ", username=" + username + ", password=" + password + "]";
     }
 
 }
--- client/src/main/java/com/guilherme/todo_app/security/jwt/TokenController.java
+++ client/src/main/java/com/guilherme/todo_app/security/jwt/TokenController.java
@@ -1,5 +1,7 @@
 package com.guilherme.todo_app.security.jwt;
 
+import java.util.Optional;
+
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
@@ -8,20 +10,30 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.guilherme.todo_app.model.users.User;
+import com.guilherme.todo_app.model.users.UserRepository;
+
 @RestController
 @RequestMapping("/auth")
 public class TokenController {
     
     private final TokenService TokenService;
+    private final UserRepository userRepository;
     private final AuthenticationManager authenticationManager;
-
-    public TokenController(TokenService TokenService, AuthenticationManager authenticationManager) {
+    
+    public TokenController(TokenService TokenService, AuthenticationManager authenticationManager, UserRepository userRepository) {
         this.TokenService = TokenService;
+        this.userRepository = userRepository;
         this.authenticationManager = authenticationManager;
     }
 
     @PostMapping("/login")
-    public ResponseEntity<JWTTokenResponse> generateToken(@RequestBody JWTTokenRequest jwtTokenRequest) {
+    public ResponseEntity<?> generateToken(@RequestBody JWTTokenRequest jwtTokenRequest) {
+
+        Optional<User> user = userRepository.findByEmail(jwtTokenRequest.getEmail());
+        if(user.isEmpty()) {
+            return ResponseEntity.of(user);
+        }
 
         var authenticationToken =
             new UsernamePasswordAuthenticationToken(
