a2942bf1cfe6b31ac5e06875d874a87573d81d1d
Guilherme Silva <133605425+GuilhermeSP01@users.noreply.github.com>
2024-10-04 12:05:41 -0300

    setup react router

--- server/src/main.tsx
+++ server/src/main.tsx
@@ -1,10 +1,26 @@
+import './index.css'
+
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+
+import HomePage from './routes/HomePage';
+import LoginPage from './routes/auth/LoginPage';
+import NotFoundPage from './routes/NotFoundPage';
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <HomePage />,
+    errorElement: <NotFoundPage />
+  }, {
+    path: '/login',
+    element: <LoginPage />
+  }
+]);
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </StrictMode>,
 )
