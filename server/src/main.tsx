01cb9f61f2a40508330363f8a68d98e49c3b634a
Guilherme Silva <133605425+GuilhermeSP01@users.noreply.github.com>
2024-10-05 12:53:37 -0300

    add login page design

--- server/src/main.tsx
+++ server/src/main.tsx
@@ -3,7 +3,7 @@ import './index.css'
 import { createBrowserRouter, RouterProvider } from 'react-router-dom'
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-
+import { ThemeProvider } from './ThemeProvider';
 import HomePage from './routes/HomePage';
 import NotFoundPage from './routes/NotFoundPage';
 import LoginPage from './routes/auth/LoginPage';
@@ -21,6 +21,8 @@ const router = createBrowserRouter([
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <RouterProvider router={router} />
+    <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
+      <RouterProvider router={router} />
+    </ThemeProvider>
   </StrictMode>,
 )
