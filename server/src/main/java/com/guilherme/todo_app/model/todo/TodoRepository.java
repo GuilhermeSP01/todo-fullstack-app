a9ea23127b7c4ea59bf1f2a1012d6bf91d8f1fa1
Guilherme Silva <133605425+GuilhermeSP01@users.noreply.github.com>
2024-10-11 07:41:55 -0300

    add findByUser on Todo repository

--- server/src/main/java/com/guilherme/todo_app/model/todo/TodoRepository.java
+++ server/src/main/java/com/guilherme/todo_app/model/todo/TodoRepository.java
@@ -1,7 +1,11 @@
 package com.guilherme.todo_app.model.todo;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import com.guilherme.todo_app.model.user.User;
+
 public interface TodoRepository extends JpaRepository<Todo, Long> {
-    
+    public List<Todo> findByUser(User user);
 }
