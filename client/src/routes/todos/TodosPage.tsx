9f2e2d3df62302ef9f8383b4cbb50877ee36465d
Guilherme Silva <133605425+GuilhermeSP01@users.noreply.github.com>
2024-10-12 10:48:55 -0300

    add list for todos

--- client/src/routes/todos/TodosPage.tsx
+++ client/src/routes/todos/TodosPage.tsx
@@ -1,6 +1,14 @@
 import fetchTodos from "@/components/hooks/fetchTodos";
 import { Button } from "@/components/ui/button";
 
+interface Todo {
+    id: number
+    title: string
+    description: string
+    targetDate: string
+    done: boolean
+}
+
 export default function TodosPage() {
 
     const data = fetchTodos();
@@ -12,6 +20,14 @@ export default function TodosPage() {
     return (
         <main>
             <Button onClick={handleClick}> Todos </Button>
+
+            <ul>
+                {data?.map((data: Todo) => (
+                    <li key={data.id}>
+                        {data.title}, {data.description}, {data.targetDate}, {data.done.toString()}
+                    </li>
+                ))}
+            </ul>
         </main>
     )
 }
